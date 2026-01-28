f5cb920af94239cf1233be6277d0f64fa776bafc
tuhnps98 <tuhnps98@gmail.com>
2026-01-28 11:04:17 +0700

    Update typeorm.config.ts

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,13 +3,14 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('typeorm', () => ({
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT) || 5432,
+  // 👇 SỬA DÒNG NÀY: Thêm || '5432' vào trong parseInt
+  port: parseInt(process.env.DB_PORT || '5432'), 
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: true, // Tự động tạo bảng (quan trọng cho lần đầu chạy)
+  synchronize: true, 
   ssl: {
-    rejectUnauthorized: false, // [QUAN TRỌNG] Dòng này giúp fix lỗi "terminated unexpectedly"
+    rejectUnauthorized: false, 
   },
 }));
