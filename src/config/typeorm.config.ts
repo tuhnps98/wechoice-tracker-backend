129cac756642d511c7ceb77120cd42a815b68ae0
tuhnps98 <tuhnps98@gmail.com>
2026-01-28 10:56:05 +0700

    Update typeorm.config.ts

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,24 +1,15 @@
 import { registerAs } from '@nestjs/config';
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { join } from 'path';
 
-export default registerAs(
-  'typeorm',
-  (): TypeOrmModuleOptions => ({
-    type: 'postgres',
-    url: process.env.DB_URL,
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || '5432', 10),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    autoLoadEntities: true,
-    synchronize: true,
-    migrations: [join(__dirname, '../..', 'migrations', '*.{js,ts}')],
-    migrationsRun: false,
-    extra: {
-      max: 3,
-      connectionTimeoutMillis: 5000,
-    },
-  }),
-);
+export default registerAs('typeorm', () => ({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true, // Tự động tạo bảng (quan trọng cho lần đầu chạy)
+  ssl: {
+    rejectUnauthorized: false, // [QUAN TRỌNG] Dòng này giúp fix lỗi "terminated unexpectedly"
+  },
+}));
