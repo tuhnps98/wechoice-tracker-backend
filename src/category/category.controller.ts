20ac78aa299c609c9a91f8fdb3f1d0321213843f
tuhnps98 <tuhnps98@gmail.com>
2026-01-28 12:29:31 +0700

    Update category.controller.ts

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,18 +1,8 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Post,
-  Delete,
-  Put,
-} from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { Category } from './category.entity';
-import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
 
-@Controller('category')
+@Controller('categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
@@ -21,28 +11,13 @@ export class CategoryController {
     return this.categoryService.findAll();
   }
 
+  // 👇 Quan trọng: Bỏ ParseIntPipe, để id là string
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<Category> {
-    return this.categoryService.findOne(id);
-  }
-
-  @Post()
-  async create(
-    @Body() createCategoryDto: CreateCategoryDto,
-  ): Promise<Category> {
-    return this.categoryService.create(createCategoryDto);
-  }
-
-  @Put(':id')
-  async update(
-    @Param('id') id: string,
-    @Body() updateCategoryDto: UpdateCategoryDto,
-  ): Promise<Category> {
-    return this.categoryService.update(id, updateCategoryDto);
-  }
-
-  @Delete(':id')
-  async remove(@Param('id') id: number): Promise<void> {
-    return this.categoryService.remove(id);
+    const category = await this.categoryService.findOne(id);
+    if (!category) {
+      throw new NotFoundException(`Category #${id} not found`);
+    }
+    return category;
   }
 }
